677a8d4f291d5b99e0100c8f2b8fdf7877ddf3d3
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-07 11:08:37 +0600

    add product collection

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
+// user inventoWarehouse
+// pass 9VlOgcVhJUbmz83p
 // Middleware
 app.use(cors())
 app.use(express.json())
@@ -13,6 +16,28 @@ app.get('/', (req, res) => {
     res.send('Cash and carry site is running')
 })
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aay4n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const productsCollection = client.db('inventoWarehouse').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({})
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir)
 //Port
 app.listen(port, () => {
     console.log("cash and carry is running on port", port)
